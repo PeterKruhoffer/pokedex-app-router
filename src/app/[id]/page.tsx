c555cfa1ed11887aae19f44df57100df859ffb74
peterkruhoffer <peter.kruhoffer@gmail.com>
2024-04-07 17:11:30 +0200

    pokemon name color changed to white

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,22 +1,26 @@
-import Link from "next/link"
-import { Pokemon } from "~/components/Pokemon"
+import Link from "next/link";
+import { Pokemon } from "~/components/Pokemon";
 
 export default function PokemonPage({ params }: { params: { id: string } }) {
   return (
-    <section className="relative flex flex-col justify-center items-center text-white translate-y-20">
+    <section className="relative flex translate-y-20 flex-col items-center justify-center text-white">
       <Pokemon id={params.id} />
-      {parseInt(params.id) > 1 && <Link
-        className="top-1/3 -translate-y-1/3 left-1/4 absolute border rounded-lg px-2 text-lg shadow-inner shadow-white/50"
-        href={"/" + (parseInt(params.id) - 1)}
-      >
-        Prev
-      </Link>}
-      {parseInt(params.id) < 1010 && <Link
-        className="top-1/3 -translate-y-1/3 right-1/4 absolute border rounded-lg px-2 text-lg shadow-inner shadow-white/50"
-        href={"/" + (parseInt(params.id) + 1)}
-      >
-        Next
-      </Link>}
+      {parseInt(params.id) > 1 && (
+        <Link
+          className="absolute left-1/4 top-1/3 -translate-y-1/3 rounded-lg border px-2 text-lg shadow-inner shadow-white/50"
+          href={"/" + (parseInt(params.id) - 1)}
+        >
+          Prev
+        </Link>
+      )}
+      {parseInt(params.id) < 1010 && (
+        <Link
+          className="absolute right-1/4 top-1/3 -translate-y-1/3 rounded-lg border px-2 text-lg shadow-inner shadow-white/50"
+          href={"/" + (parseInt(params.id) + 1)}
+        >
+          Next
+        </Link>
+      )}
     </section>
-  )
+  );
 }
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,57 +1,57 @@
-"use client"
-import Image from "next/image"
-import type { Pokemon } from "pokenode-ts"
-import { useQuery } from "@tanstack/react-query"
-import { cn } from "~/util"
+"use client";
+import Image from "next/image";
+import type { Pokemon } from "pokenode-ts";
+import { useQuery } from "@tanstack/react-query";
+import { cn } from "~/util";
 
-const colorMap = new Map<string, string>()
-colorMap.set("normal", "#A8A878")
-colorMap.set("fire", "#F08030")
-colorMap.set("water", "#6890F0")
-colorMap.set("electric", "#F8D030")
-colorMap.set("grass", "#78C850")
-colorMap.set("ice", "#98D8D8")
-colorMap.set("fighting", "#C03028")
-colorMap.set("poison", "#A040A0")
-colorMap.set("ground", "#E0C068")
-colorMap.set("flying", "#A890F0")
-colorMap.set("psychic", "#F85888")
-colorMap.set("bug", "#A8B820")
-colorMap.set("rock", "#B8A038")
-colorMap.set("ghost", "#705898")
-colorMap.set("dragon", "#7038F8")
-colorMap.set("dark", "#705848")
-colorMap.set("steel", "#B8B8D0")
-colorMap.set("fairy", "#EE99AC")
+const colorMap = new Map<string, string>();
+colorMap.set("normal", "#A8A878");
+colorMap.set("fire", "#F08030");
+colorMap.set("water", "#6890F0");
+colorMap.set("electric", "#F8D030");
+colorMap.set("grass", "#78C850");
+colorMap.set("ice", "#98D8D8");
+colorMap.set("fighting", "#C03028");
+colorMap.set("poison", "#A040A0");
+colorMap.set("ground", "#E0C068");
+colorMap.set("flying", "#A890F0");
+colorMap.set("psychic", "#F85888");
+colorMap.set("bug", "#A8B820");
+colorMap.set("rock", "#B8A038");
+colorMap.set("ghost", "#705898");
+colorMap.set("dragon", "#7038F8");
+colorMap.set("dark", "#705848");
+colorMap.set("steel", "#B8B8D0");
+colorMap.set("fairy", "#EE99AC");
 
 function getPokeColor(type: string | undefined) {
-  return colorMap.get(type ?? "normal")
+  return colorMap.get(type ?? "normal");
 }
 
 async function getPokemon(poke: string): Promise<Pokemon> {
-  const res = await fetch("https://pokeapi.co/api/v2/pokemon/" + poke)
-  const data = await res.json() as Pokemon
-  return data
+  const res = await fetch("https://pokeapi.co/api/v2/pokemon/" + poke);
+  const data = (await res.json()) as Pokemon;
+  return data;
 }
 
-export function PokemonCard(props: { id: string, className?: string }) {
-  const { data } = useQuery({
+export function PokemonCard(props: { id: string; className?: string }) {
+  const { data, isLoading } = useQuery({
     queryKey: ["pokemon", props.id],
-    queryFn: () => getPokemon(props.id.toLowerCase())
-  })
-  if (!data) {
-    return <div>Loading...</div>
+    queryFn: () => getPokemon(props.id.toLowerCase()),
+  });
+  if (!data || isLoading) {
+    return <div>Loading...</div>;
   }
 
   return (
     <div
       className={cn(
-        "h-72 w-full rounded-lg flex flex-col justify-center items-center p-4 shadow-inner shadow-white/50",
-        props.className
+        "flex h-72 w-full flex-col items-center justify-center rounded-lg p-4 shadow-inner shadow-white/50",
+        props.className,
       )}
       style={{ backgroundColor: getPokeColor(data.types.at(0)?.type.name) }}
     >
-      <h2 className="text-lg uppercase">{data.name}</h2>
+      <h2 className="text-lg uppercase text-white">{data.name}</h2>
       <Image
         src={data.sprites.front_default ?? ""}
         alt={data.name}
@@ -63,7 +63,7 @@ export function PokemonCard(props: { id: string, className?: string }) {
         {data.types.map((type) => (
           <span
             key={type.type.name}
-            className="uppercase rounded-xl px-2 py-1 border shadow-inner shadow-white/50"
+            className="rounded-xl border px-2 py-1 uppercase shadow-inner shadow-white/50"
             style={{ backgroundColor: getPokeColor(type.type.name) }}
           >
             {type.type.name}
@@ -71,5 +71,5 @@ export function PokemonCard(props: { id: string, className?: string }) {
         ))}
       </div>
     </div>
-  )
+  );
 }
